f5b7f70016894b191155e5166f7d9a4725862224
h2ash <hun22ash@gmail.com>
2020-02-25 02:47:21 +0200

    Updates

---
+++ precache-manifest.52c9c152044f1acf96082f78fbebd422.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1823b059ec729002fc8d28762510cfd0",
+    "url": "/redux-saga-first/index.html"
+  },
+  {
+    "revision": "ca4cf82563336ae514c8",
+    "url": "/redux-saga-first/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "4156585baaf4144dd2d6",
+    "url": "/redux-saga-first/static/js/2.10534b4a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/redux-saga-first/static/js/2.10534b4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca4cf82563336ae514c8",
+    "url": "/redux-saga-first/static/js/main.5af1030b.chunk.js"
+  },
+  {
+    "revision": "3f6fcf9cb16203bfcc12",
+    "url": "/redux-saga-first/static/js/runtime-main.ca2149ed.js"
+  }
+]);
